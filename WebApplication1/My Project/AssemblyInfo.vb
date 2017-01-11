f05829abd1cc0cd0b41ef0e0858f28224381d1ba
wangshansong <876561691@qq.com>
2017-01-11 21:17:43 +0800

    111

---
+++ WebApplication1/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的常规信息通过下列特性集
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+' 检查程序集特性的值
+<Assembly: AssemblyTitle("WebApplication1")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("WebApplication1")> 
+<Assembly: AssemblyCopyright("版权所有(C)  2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+<Assembly: Guid("70670046-1d80-4c1e-98e6-c76e2242c242")> 
+
+' 程序集的版本信息由下列四个值组成:
+'
+'      主版本
+'      次版本
+'      内部版本号
+'      修订号
+'
+' 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
+' 方法是按如下所示使用“*”:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
