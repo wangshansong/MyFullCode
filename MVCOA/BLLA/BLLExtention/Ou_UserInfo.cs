79f5dfceb690a9cc5e51cafde493d3a5a3b73fdd
wangshansong <876561691@qq.com>
2017-01-13 14:21:56 +0800

    提交Ou_UserInfo类

---
+++ MVCOA/BLLA/BLLExtention/Ou_UserInfo.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLLA
+{
+    public partial class Ou_UserInfo:IBLL.IOu_UserInfoBLL
+    {
+        /// <summary>
+        /// 登录验证方法
+        /// </summary>
+        /// <param name="strName">用户名</param>
+        /// <param name="strPwd">密码</param>
+        /// <returns></returns>
+        public MODEL.Ou_UserInfo Login(string strName, string strPwd)
+        {
+            MODEL.Ou_UserInfo user = base.GetListBy(p=>p.uLoginName == strName).FirstOrDefault();
+            if (user != null && user.uPwd == DataHelper.MD5(strPwd))
+            {
+                return user.ToPOCO();
+            }
+            return null;
+        }
+    }
+}
