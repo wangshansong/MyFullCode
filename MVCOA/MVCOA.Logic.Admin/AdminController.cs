662ee863b94a9f01cefd4765d3b53d312137b656
wangshansong <876561691@qq.com>
2017-01-13 13:19:30 +0800

    增加记住用户名

--- MVCOA/MVCOA.Logic.Admin/AdminController.cs
+++ MVCOA/MVCOA.Logic.Admin/AdminController.cs
@@ -44,11 +44,14 @@ namespace MVCOA.Logic.Admin
                 //1.1登录成功之后将数据写入到Session中
                 Session["ainfo"] = user;
                 //1.2登录成功之后将数据写入到Cookie中
-                string strEncryptUserInfo = SecurityHelper.EncryptUserInfo(user.uLoginName);
-                HttpCookie userInfoCookie = new HttpCookie("aInfo",strEncryptUserInfo);
-                userInfoCookie.Expires = DateTime.Now.AddDays(1);//设置失效时间
-                // userInfoCookie.Path = "/admin/";
-                Response.Cookies.Add(userInfoCookie);
+                if (!string.IsNullOrEmpty(collection["isAllway"]))
+                {
+                    string strEncryptUserInfo = SecurityHelper.EncryptUserInfo(user.uLoginName);
+                    HttpCookie userInfoCookie = new HttpCookie("aInfo", strEncryptUserInfo);
+                    userInfoCookie.Expires = DateTime.Now.AddDays(1);//设置失效时间
+                    // userInfoCookie.Path = "/admin/";
+                    Response.Cookies.Add(userInfoCookie);
+                } 
                 //1.3返回登录成功信息
                 ajaxMsg.Msg = "登录成功！";
                 ajaxMsg.Statu = "ok";
