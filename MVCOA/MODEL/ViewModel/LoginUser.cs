e3f349e2e652c00e32e031e2e662de8da7e77547
wangshansong <876561691@qq.com>
2017-01-20 13:39:03 +0800

    新增ViewModel->LoginUser

---
+++ MVCOA/MODEL/ViewModel/LoginUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL.ViewModel
+{
+    /// <summary>
+    /// 用户登录模型
+    /// </summary>
+    public class LoginUser
+    {
+        /// <summary>
+        /// 登录用户命名
+        /// </summary>
+        public string uLoginName { get; set; }
+        /// <summary>
+        /// 登录密码
+        /// </summary>
+        public string uPwd { get; set; }
+        /// <summary>
+        /// 是否记住用户名 
+        /// </summary>
+        public bool isalways { get; set; }
+    }
+}
