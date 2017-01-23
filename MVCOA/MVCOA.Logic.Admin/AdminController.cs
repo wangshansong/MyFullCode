85c39202fc887b01db15a6c89238b8e4a3365444
wangshansong <876561691@qq.com>
2017-01-23 20:47:00 +0800

    1

--- MVCOA/MVCOA.Logic.Admin/AdminController.cs
+++ MVCOA/MVCOA.Logic.Admin/AdminController.cs
@@ -32,29 +32,34 @@ namespace MVCOA.Logic.Admin
         /// </summary>
         /// <returns></returns>
         [HttpPost]
-        public ActionResult Login(FormCollection collection)
+        public ActionResult Login(MODEL.ViewModel.LoginUser userInfo)
         {
-            AjaxMsgModel ajaxMsg = new AjaxMsgModel();
+            AjaxMsgModel ajaxMsg = new AjaxMsgModel { Statu = "err", Msg = "登录失败" };
             //1.获取用户名密码
-            string strName = collection["txtName"].ToString();
-            string strPwd = collection["txtPwd"].ToString();
+            string strName = userInfo.uLoginName;
+            string strPwd = userInfo.uPwd;
             MODEL.Ou_UserInfo user = OperateContext.BLLSession.IOu_UserInfoBLL.Login(strName, strPwd);
 
             if (user != null)
             {
-              List<MODEL.Ou_Permission> plist =    OperateContext.GetUserPermission(user.uId);
+            
                 //1.1登录成功之后将数据写入到Session中
                 Session["ainfo"] = user;
                 //1.2登录成功之后将数据写入到Cookie中
-                if (!string.IsNullOrEmpty(collection["isAllway"]))
+                if (userInfo.isAlways)
                 {
                     string strEncryptUserInfo = SecurityHelper.EncryptUserInfo(user.uLoginName);
                     HttpCookie userInfoCookie = new HttpCookie("aInfo", strEncryptUserInfo);
                     userInfoCookie.Expires = DateTime.Now.AddDays(1);//设置失效时间
                     // userInfoCookie.Path = "/admin/";
                     Response.Cookies.Add(userInfoCookie);
-                } 
-                //1.3返回登录成功信息
+                }
+
+                //1.3查询当前用户的权限信息存入到Session中
+                List<MODEL.Ou_Permission> plist = OperateContext.GetUserPermission(user.uId);
+                Session["aPermission"] = plist;
+
+                //1.4返回登录成功信息
                 ajaxMsg.Msg = "登录成功！";
                 ajaxMsg.Statu = "ok";
                 ajaxMsg.BackUrl = "/admin/admin/Index";
