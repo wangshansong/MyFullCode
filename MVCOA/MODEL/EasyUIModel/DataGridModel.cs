f30090861967a978aaba5f3b2c0a5cc5f94cd0dd
wangshansong <876561691@qq.com>
2017-02-20 14:21:43 +0800

    22

---
+++ MVCOA/MODEL/EasyUIModel/DataGridModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL.EasyUIModel
+{
+    public  class DataGridModel
+    {
+        /// <summary>
+        /// 数据总条数
+        /// </summary>
+        public int total { set; get; }
+        /// <summary>
+        /// Json格式的数据内容
+        /// </summary>
+        public object rows { set; get; }
+        /// <summary>
+        /// 脚
+        /// </summary>
+        public object footer { set; get; }
+    }
+}
---
+++ MVCOA/MVCOA.Logic.Admin/SysController.cs
@@ -0,0 +1,43 @@
+﻿using MODEL.EasyUIModel;
+using MVCOA.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVCOA.Logic.Admin
+{
+    public class SysController : Controller
+    {
+        [HttpGet]
+        public ActionResult Permission()
+        {
+            return View();
+        }
+
+        public ActionResult GetPermData()
+        {
+            var pList = OperateContext.Current.BLLSession.IOu_PermissionBLL.GetListBy(p => p.pIsShow == true && p.pIsDel == false).Select(p=>p.ToPOCO());
+
+            DataGridModel datagridModel = new DataGridModel()
+            {
+                total = (pList == null ? 0 : pList.Count()),
+                rows = pList,
+                footer = null
+            };
+
+            return Json(datagridModel);
+        }
+
+    [HttpPost]
+        public ActionResult Editpermission(int ID)
+        {
+            var pList = OperateContext.Current.BLLSession.IOu_PermissionBLL.GetListBy(p =>p.pid == ID).Select(p => p.ToPOCO());
+            
+            return null;
+        }
+        
+    }
+}
